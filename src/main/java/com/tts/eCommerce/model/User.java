3b7da051393c77f90bd0b8d42efb4ee34229fef0
KatBerman <65688281+KatBerman@users.noreply.github.com>
2020-07-07 12:06:24 -0400

    build user model

--- src/main/java/com/tts/eCommerce/model/User.java
+++ src/main/java/com/tts/eCommerce/model/User.java
@@ -1,5 +1,60 @@
 package com.tts.eCommerce.model;
-
+@Entity
 public class User {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="user.id")
+	private Long id;
+	
+	private String email;
+	private String firstName;
+	private String lastName;
+	private String username;
+	private String password;
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Long getId() {
+		return id;
+	}
+	
+	public User(Long id, String email, String firstName, String lastName, String username, String password) {
+		this.id = id;
+		this.email = email;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.username = username;
+		this.password = password;
+	}
+	
+	
+	
 
 }
