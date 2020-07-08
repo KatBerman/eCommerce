3a1e4888d74b08b6812943f9e66e6d6ad9c277e7
KatBerman <65688281+KatBerman@users.noreply.github.com>
2020-07-08 10:47:10 -0400

    comment in user service

--- src/main/java/com/tts/eCommerce/service/UserService.java
+++ src/main/java/com/tts/eCommerce/service/UserService.java
@@ -44,7 +44,7 @@ public class UserService {
 
 	public User saveNewUser(User user) {
 	  user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
-	  user.setActive(1);
+//	  user.setActive(1);
 	  Role userRole = roleRepository.findByRole("USER");
 	  user.setRoles(new HashSet<Role>(Arrays.asList(userRole)));
 	  return userRepository.save(user);
