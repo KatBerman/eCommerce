5adb97ffc13150a7ccb4fefea936569927d954f5
KatBerman <65688281+KatBerman@users.noreply.github.com>
2020-07-08 11:03:14 -0400

    updated authorization controller

--- src/main/java/com/tts/eCommerce/controller/AuthorizationController.java
+++ src/main/java/com/tts/eCommerce/controller/AuthorizationController.java
@@ -1,5 +1,45 @@
 package com.tts.eCommerce.controller;
 
-public class AuthorizationController {
+import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.tts.eCommerce.model.User;
+import com.tts.eCommerce.service.UserService;
+
+@Controller
+public class AuthorizationController {
+	@Autowired
+	private UserService userService;
+
+	@GetMapping(value = "/login")
+	public String login() {
+		return "login";
+	}
+
+	@GetMapping(value = "/signup")
+	public String registration(Model model) {
+		User user = new User();
+		model.addAttribute("user", user);
+		return "signup";
+	}
+
+	@PostMapping(value = "/signup")
+	public String createNewUser(@Valid User user, BindingResult bindingResult, Model model) {
+		User userExists = userService.findByEmail(user.getEmail());
+		if (userExists != null) {
+			bindingResult.rejectValue("email", "error.user", "That email already has an account");
+		}
+		if (!bindingResult.hasErrors()) {
+			userService.saveNewUser(user);
+			model.addAttribute("success", "Sign up successful!");
+			model.addAttribute("user", new User());
+		}
+		return "signup";
+	}
 }
