e2d448090f8143475c948a61bb364cd11e10665b
KatBerman <65688281+KatBerman@users.noreply.github.com>
2020-07-08 08:44:41 -0400

    build user repository

--- src/main/java/com/tts/eCommerce/repository/UserRepository.java
+++ src/main/java/com/tts/eCommerce/repository/UserRepository.java
@@ -1,7 +1,13 @@
 package com.tts.eCommerce.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+import com.tts.eCommerce.model.User;
 
+public interface UserRepository extends CrudRepository<User, Long> {
+	User findByUsername(String username);
+	List<User> findUsersByRole(String role);
+	User findByEmail(String email);
 }
