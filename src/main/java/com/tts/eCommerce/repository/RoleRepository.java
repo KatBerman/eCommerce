6c8b2fd11ff2d90016e11fd8cc65b615987b51b8
lenadecker <lenamdecker@gmail.com>
2020-07-07 20:19:42 -0500

    add role.java and roleRepository

--- src/main/java/com/tts/eCommerce/repository/RoleRepository.java
+++ src/main/java/com/tts/eCommerce/repository/RoleRepository.java
@@ -1,7 +1,12 @@
 package com.tts.eCommerce.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+import com.tts.eCommerce.model.Role;
+
+@Repository
 public interface RoleRepository extends CrudRepository<Role, Long> {
+	Role findByRole(String role);
 
 }
