629ad7be43e64b5656503e8dcb7938dcd57830bf
KatBerman <65688281+KatBerman@users.noreply.github.com>
2020-07-09 09:24:00 -0400

    update cart controller

--- src/main/java/com/tts/eCommerce/controller/CartController.java
+++ src/main/java/com/tts/eCommerce/controller/CartController.java
@@ -2,12 +2,15 @@ package com.tts.eCommerce.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.tts.eCommerce.model.Cart;
 import com.tts.eCommerce.model.Product;
 import com.tts.eCommerce.service.ProductService;
 import com.tts.eCommerce.service.UserService;
@@ -19,6 +22,9 @@ public class CartController {
 	
 	@Autowired
 	private UserService userService;
+	
+//	@Autowired
+//	private CartService cartService;
 	
 	@GetMapping("/cart")
 	public String showCart() {
@@ -40,6 +46,14 @@ public class CartController {
 	  }
 	  return "storefront/cart";
 	}
+	
+	@RequestMapping("/storefront")
+		@GetMapping("/cart")
+		public String viewCart(Cart cart, Model model) {
+			model.addAttribute("cart", cart);
+			return "storefront/cart";
+		}
+	
 
 	@DeleteMapping("/cart")
 	public String removeFromCart(@RequestParam long id) {
