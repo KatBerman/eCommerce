6a78d9bcf3cc1dc863ab7f645f08060f6c9a0721
lenadecker <lenamdecker@gmail.com>
2020-07-11 19:37:25 -0500

    update orderService and orderController

--- src/main/java/com/tts/eCommerce/service/OrderService.java
+++ src/main/java/com/tts/eCommerce/service/OrderService.java
@@ -2,11 +2,24 @@ package com.tts.eCommerce.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.tts.eCommerce.model.Cart;
+import com.tts.eCommerce.model.Order;
 import com.tts.eCommerce.repository.OrderRepository;
 
 public class OrderService {
 	
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	public Order makePurchase(Cart cart) {
+		//Need code here - not sure what the method would be
+		return null;
+	}
+	
+	
+	private Double findSalesTax(Double subtotal) {
+		Double tax = 0.1 * subtotal;
+		return tax;
+	}
 
 }
