366de66541d367bc4bd4f82b1f426ce2151bb3be
lenadecker <lenamdecker@gmail.com>
2020-07-11 19:03:09 -0500

    add to adminController

--- src/main/java/com/tts/eCommerce/controller/AdminController.java
+++ src/main/java/com/tts/eCommerce/controller/AdminController.java
@@ -1,5 +1,25 @@
 package com.tts.eCommerce.controller;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.tts.eCommerce.model.Product;
+import com.tts.eCommerce.service.ProductService;
+
+@Controller
 public class AdminController {
+	
+	@Autowired
+	private ProductService productService;
+	
+	@RequestMapping(value = {"/products/new", "/products/{productId}/edit"}, method = {RequestMethod.POST, RequestMethod.PUT })
+	public String createOrUpdate(@Valid Product product) {
+	  productService.save(product);
+	  return "redirect:/admin/products/" + product.getId();
+	}
 
 }
