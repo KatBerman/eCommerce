adf376409440b64cd14a21a4c608f77e24046c7f
lenadecker <lenamdecker@gmail.com>
2020-07-11 19:45:38 -0500

    update orderService

--- src/main/java/com/tts/eCommerce/service/OrderService.java
+++ src/main/java/com/tts/eCommerce/service/OrderService.java
@@ -1,11 +1,13 @@
 package com.tts.eCommerce.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.tts.eCommerce.model.Cart;
 import com.tts.eCommerce.model.Order;
 import com.tts.eCommerce.repository.OrderRepository;
 
+@Service
 public class OrderService {
 	
 	@Autowired
